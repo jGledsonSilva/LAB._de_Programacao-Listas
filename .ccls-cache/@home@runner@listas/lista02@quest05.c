a227013e7453676b19f6bd5693348b3e2bfa9c29
Gledson Silva <99442160+Gledson-silv4@users.noreply.github.com>
2022-04-18 22:45:31 +0000

    main

--- .ccls-cache/@home@runner@listas/lista02@quest05.c
+++ .ccls-cache/@home@runner@listas/lista02@quest05.c
@@ -2,7 +2,22 @@
 
 int main(){
 
-  
+  int num, maior=-1, menor;
+
+  printf("digite um valor ou digite 0 para sair:");
+  scanf("%d",&num);
+
+  while (num != 0){
+    printf("\ndigite um valor ou digite 0 para sair:");
+    scanf("%d",&num);
+
+    if (num>maior){
+      maior = num;
+    }
+    else menor = num;
+  }
+
+  printf("\nO numero maior é: %d e o menor é: %d.\n\n", maior, menor);
   
   return 0;
 }
