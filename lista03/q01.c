628c92296c5e3cbc97beb0af45b79529504e2f5f
Gledson-silv4 <gledson.silva60@aluno.ifce.edu.br>
2022-04-26 19:44:28 -0300

    main

---
+++ lista03/q01.c
@@ -0,0 +1,11 @@
+/* Faça um programa em C que leia um vetor do tipo float com 15 elementos e apresente a soma do
+menor e maior elemento do vetor fornecido. */ 
+
+#include <stdio.h>
+
+int main(){
+
+  
+  
+  return 0;
+}
