8abd170fabb9a839ae4a0e3e02f1738a724214b1
Gledson-silv4 <gledson.silva60@aluno.ifce.edu.br>
2022-04-28 21:00:41 -0300

    questões

--- lista03/q02.c
+++ lista03/q02.c
@@ -1,2 +1,24 @@
 /* Faça um programa em C que leia uma string e um caractere do usuário e informe se a string de entrada contém o caracter fornecido. */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    char str[100], caracter;
+    int i;
+
+    system("clear");
+
+    printf("Digite a string: ");
+    scanf("%100[^\n]", str);
+    getchar();
+
+    printf("Digite um caracter: ");
+    scanf("%c", &caracter);
+
+    for(i=0; str[i]!='\0'; i++);
+    str[i]==caracter? printf("A string contem o caracter fornecido\n"):printf("A string nao contem o caracter fornecido.\n");
+
+    return 0;
+}
--- lista03/q03.c
+++ lista03/q03.c
@@ -1,2 +1,18 @@
 /* Faça um programa em C que leia uma string do usuário e informe a quantidade de caracteres da string fornecida. Não use a função strlen(). */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    char str[100];
+    int i;
+
+    printf("Digite a string: ");
+    scanf("%100[^\n]", str);
+
+    for(i=0; str[i]!='\0'; i++);
+    printf("Tem %d caracteres.\n\n", i);
+
+    return 0;
+}
