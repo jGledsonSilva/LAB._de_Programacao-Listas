215d2a92d9fde5f8b06d1446f502292bc6260489
Gledson-silv4 <gledson.silva60@aluno.ifce.edu.br>
2022-05-27 22:18:12 -0300

    questões

--- lista04/quest03.c
+++ lista04/quest03.c
@@ -2,10 +2,30 @@
 valor fornecido. Crie um critério para finalização do programa. Utilize ponteiros. */ 
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-  
+  float a, menor, i = 0, *pa = NULL, *pmenor = NULL;
+
+  pa = &a;
+  pmenor = &menor;
+
+  system("clear");
+
+  do{
+    printf("\nDigite o valor (Digite [0] para finalizar): ");
+    scanf("%f", pa);
+    
+    if (*pa == 0)break;
+    
+    ((i == 0) || (*pa < *pmenor))? *pmenor = *pa, i++: *pmenor;
+    
+    printf("\no menor valor: %.2f\n", *pmenor);   
+    
+  }while(1);
+
+   printf("\no menor valor: %.2f\n", *pmenor); 
   
   return 0;
 }
